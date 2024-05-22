6e1fcf5d2324524ae42d4d6211d00af220cf711b
Mina-Emad-101 <menamanos@gmail.com>
2024-05-22 21:43:08 +0300

    ide_helper

--- app/Models/Priority.php
+++ app/Models/Priority.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|Priority newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Priority newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Priority query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Priority whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Priority whereName($value)
+ * @mixin \Eloquent
+ */
 class Priority extends Model
 {
     use HasFactory;
