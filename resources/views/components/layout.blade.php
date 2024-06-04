04adad803a9b9faf6ba7a053a6b49937456ee48e
Mina-Emad-101 <menamanos@gmail.com>
2024-06-04 04:34:23 +0300

    UI Buttons for Register

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,16 +11,27 @@
 <body>
     <div class="min-h-full">
         <nav class="bg-gray-800">
-            <div class="flex items-center mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16">
-                <div class="flex-shrink-0">
-                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company">
+            <div class="flex justify-between">
+                <div class="flex items-center max-w-7xl px-4 sm:px-6 lg:px-8 h-16">
+                    <div class="flex-shrink-0">
+                        <img class="h-8 w-8" src="https://www.svgrepo.com/show/375429/error-reporting.svg"
+                            alt="Bug Tracker">
+                    </div>
+                    <div class="ml-3 flex items-baseline space-x-4 block">
+                        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                        <x-nav-link href="/bugs" :active="request()->is('bugs')">Bugs</x-nav-link>
+                        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                    </div>
                 </div>
-                <div class="ml-3 flex items-baseline space-x-4 block">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                    <x-nav-link href="/bugs" :active="request()->is('bugs')">Bugs</x-nav-link>
-                    <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <div class="flex items-center max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16">
+                    <div class="ml-3 flex items-baseline space-x-4 block">
+                        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                        @auth
+                            <x-nav-link href="/login" :active="request()->is('login')">Login</x-nav-link>
+                            <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+                        @endauth
+                    </div>
                 </div>
             </div>
         </nav>
