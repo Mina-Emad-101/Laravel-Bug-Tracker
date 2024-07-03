1b3e9edebbd145cdb9ec7fee5490d9a40e8b61f7
Mina-Emad-101 <menamanos@gmail.com>
2024-07-03 15:44:57 +0300

    About and Home

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <x-header>About</x-header>
     <x-main>
-        <h1>Hello, About!</h1>
+        <h1>This App helps with Bug Tracking of Multiple Projects by (Admins, Staff and Customers)</h1>
+        <h1>For more details visit: <a href="https://github.com/Mina-Emad-101/Laravel-Bug-Tracker">My Github Repo</a></h1>
     </x-main>
 </x-layout>
