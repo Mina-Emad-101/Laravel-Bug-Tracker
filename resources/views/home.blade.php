08954bac089ee7f5d8c3b56150f86c60ba3018c3
Mina-Emad-101 <menamanos@gmail.com>
2024-07-18 21:55:59 +0300

    Home PPage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,13 @@
     <x-header>Home</x-header>
     <x-main>
         <h1>Welcome to Bug Tracker!</h1>
+        <br>
+        <br>
+        <h1>To Interact with the App and try the 3 roles available:</h1>
+        <ul>
+            <li>Admin: admin@gmail.com, admin1234</li>
+            <li>Staff: staff@gmail.com, staff1234</li>
+            <li>Customer: customer@gmail.com, customer1234</li>
+        </ul>
     </x-main>
 </x-layout>
