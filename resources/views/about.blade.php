b89805a13da189979d37281f53fcf93c7be9a54e
Mina-Emad-101 <menamanos@gmail.com>
2024-07-18 22:08:39 +0300

    UI Changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-header>About</x-header>
     <x-main>
-        <h1>This App helps with Bug Tracking of Multiple Projects by (Admins, Staff and Customers)</h1>
-        <h1>For more details visit: <a href="https://github.com/Mina-Emad-101/Laravel-Bug-Tracker">My Github Repo</a></h1>
+        <h1 class="text-2xl">This App helps with Bug Tracking of Multiple Projects by (Admins, Staff and Customers)</h1>
+        <h1 class="text-2xl">For more details visit: <a class="text-sky-500" href="https://github.com/Mina-Emad-101/Laravel-Bug-Tracker">My Github Repo</a></h1>
     </x-main>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,15 @@
 <x-layout>
     <x-header>Home</x-header>
     <x-main>
-        <h1>Welcome to Bug Tracker!</h1>
+        <h1 class="text-2xl">Welcome to Bug Tracker!</h1>
         <br>
         <br>
-        <h1>To Interact with the App and try the 3 roles available:</h1>
-        <ul>
-            <li>Admin: admin@gmail.com, admin1234</li>
-            <li>Staff: staff@gmail.com, staff1234</li>
-            <li>Customer: customer@gmail.com, customer1234</li>
+        <h1 class="text-xl">To Interact with the App and try the 3 roles available:</h1>
+        <br>
+        <ul class="text-lg ml-10">
+            <li>- Admin: admin@gmail.com, admin1234</li>
+            <li>- Staff: staff@gmail.com, staff1234</li>
+            <li>- Customer: customer@gmail.com, customer1234</li>
         </ul>
     </x-main>
 </x-layout>
